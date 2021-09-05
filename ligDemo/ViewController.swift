d4cb64e6e88c360d0334825bc03f9a2f359708e5
will <alangprs1@gmail.com>
2021-09-05 16:15:53 +0800

    Initial Commit

---
+++ ligDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ligDemo
+//
+//  Created by 翁燮羽 on 2021/9/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
